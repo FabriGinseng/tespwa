5c1f94798244ae0fec194b90ea7ddbf0462ae634
Afiume <afiume@sorint.com>
2023-09-04 14:51:46 +0200

    aggiunto onesignal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-
-createApp(App).mount('#app')
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
+//import 'OneSignalSDKWorker' 
+createApp(App).use(OneSignalVuePlugin, {
+    appId: '17a8e88e-effa-4d51-a98e-826375232323',
+  }).mount('#app')
