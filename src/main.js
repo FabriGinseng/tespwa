42f7e8b14ff375c5bb1e1e5e07742cadc2988486
Afiume <afiume@sorint.com>
2023-09-04 17:15:15 +0200

    prova

--- src/main.js
+++ src/main.js
@@ -5,6 +5,5 @@ import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 //import 'OneSignalSDKWorker' 
 createApp(App).use(OneSignalVuePlugin, {
    // appId: 'a373835c-11e4-49e8-a0b2-52a0b2e93ab8',
-   appId:"b7fced3b-c9c5-4587-83d2-67a2fe99d6bd",
-    allowLocalhostAsSecureOrigin: true,
+   appId:"b7fced3b-c9c5-4587-83d2-67a2fe99d6bd"
   }).mount('#app')
