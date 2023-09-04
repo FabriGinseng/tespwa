d18e14ba9842d36701f3b5caa1e088eb025ecab3
Afiume <afiume@sorint.com>
2023-09-04 17:36:16 +0200

    sto provando varie cose

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
-//import 'OneSignalSDKWorker' 
 createApp(App).use(OneSignalVuePlugin, {
    // appId: 'a373835c-11e4-49e8-a0b2-52a0b2e93ab8',
    appId:"b7fced3b-c9c5-4587-83d2-67a2fe99d6bd"
