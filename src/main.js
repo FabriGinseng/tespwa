b14dbbc797881e9521ebd29d9627ca0d3888d7a0
Afiume <afiume@sorint.com>
2023-09-04 15:21:28 +0200

    sto provando

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import './registerServiceWorker'
 import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 //import 'OneSignalSDKWorker' 
 createApp(App).use(OneSignalVuePlugin, {
-    appId: '17a8e88e-effa-4d51-a98e-826375232323',
+    appId: 'b7fced3b-c9c5-4587-83d2-67a2fe99d6bd',
   }).mount('#app')
