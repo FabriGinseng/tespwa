6ba2ba0d3b440db4b9b6f55b70918b3f2eb40e50
Afiume <afiume@sorint.com>
2023-09-08 15:50:50 +0200

    vediamo se ricevo le notifiche

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import OneSignal from '@onesignal/onesignal-vue3'
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/OneSignalSDKWorker.js').then((registration) => {
+    console.log('Service Worker 2 registered with scope:', registration.scope);
+  }).catch((error) => {
+    console.error('Service Worker 2 registration failed:', error);
+  });
+}
+
 const app = createApp(App, {
     productionTip: false
   })
