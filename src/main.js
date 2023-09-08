90ca770f5ce39a3650efd954ddd1225c7807bcf5
Afiume <afiume@sorint.com>
2023-09-08 16:14:16 +0200

    aggiunto di unovo in public directoriy

--- src/main.js
+++ src/main.js
@@ -3,21 +3,21 @@ import App from './App.vue'
 import './registerServiceWorker'
 import OneSignal from '@onesignal/onesignal-vue3'
 
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/push/OneSignalSDKWorker.js').then((registration) => {
+  navigator.serviceWorker.register('/push/OneSignalSDKWorker.js').then((registration) => {
     console.log('Service Worker 2 registered with scope:', registration.scope);
   }).catch((error) => {
     console.error('Service Worker 2 registration failed:', error);
   });
 }
-
 const app = createApp(App, {
-    productionTip: false
-  })
-  
-  app.use(OneSignal, {
-    appId: "b7fced3b-c9c5-4587-83d2-67a2fe99d6bd",
-    allowLocalhostAsSecureOrigin: true
-  })
-  
-  app.mount('#app')
+  productionTip: false
+})
+
+app.use(OneSignal, {
+  appId: "b7fced3b-c9c5-4587-83d2-67a2fe99d6bd",
+  allowLocalhostAsSecureOrigin: true
+})
+
+app.mount('#app')
