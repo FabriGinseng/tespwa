01d46f68c450f4aa936e2566d1a6631fe81fd3e7
Afiume <afiume@sorint.com>
2023-09-08 16:53:34 +0200

    boh

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ const app = createApp(App, {
   productionTip: false
 })
 
-app.use(OneSignal, {
-  appId: "b7fced3b-c9c5-4587-83d2-67a2fe99d6bd",
-  allowLocalhostAsSecureOrigin: true
-})
+// app.use(OneSignal, {
+//   appId: "b7fced3b-c9c5-4587-83d2-67a2fe99d6bd",
+//   allowLocalhostAsSecureOrigin: true
+// })
 
 app.mount('#app')
 
